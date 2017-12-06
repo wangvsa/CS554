d16c624ef8db8c2544b4c671b6d6bf136cd5e004
Chen Wang <wangvsa@163.com>
2017-12-06 15:39:43 -0600

    Use unordered_map to replace map

--- esc.cc
+++ esc.cc
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <stdio.h>
-#include <map>
+#include <unordered_map>
 #include <chrono>
 #include "util.h"
 #include "matrix_io.h"
@@ -9,14 +9,21 @@
 using namespace std;
 using Clock=std::chrono::high_resolution_clock;
 
+
+// Define key(i, j), convert coordinate(i, j) to a size_t value
+inline size_t key(int i, int j) {return (size_t) i << 32 | (unsigned int) j;}
+inline int get_first(size_t C) { return C>>32; }
+inline int get_second(size_t C) { return C & 0xFFFFFFFF; }
+
+
 // Scale ith row of matrix B
-void scale_csr_row(Matrix mat, float scalar, int A_row, int A_col, map<pair<int, int>, float> *C) {
+void scale_csr_row(Matrix mat, float scalar, int A_row, int A_col, unordered_map<size_t, float> *C) {
     // access ith row of B, i.e. ith col of A
     for(int i=mat.I[A_col]; i < mat.I[A_col+1]; i++) {
         int B_col = mat.J[i];
         double val = mat.val[i] * scalar;
 
-        pair<int, int> p = pair<int, int>(A_row, B_col);
+        size_t p = key(A_row, B_col);
         if((*C).find(p) == (*C).end())
         	(*C)[p] = val;
         else
@@ -25,7 +32,7 @@ void scale_csr_row(Matrix mat, float scalar, int A_row, int A_col, map<pair<int,
 }
 
 // Convert matrix stored in map to COO Matrix
-Matrix map_to_coo_matrix(map<pair<int,int>, float> m, int ROWS, int COLS) {
+Matrix map_to_coo_matrix(unordered_map<size_t, float> m, int ROWS, int COLS) {
 	Matrix mat;
 	int size = m.size();
 	mat.M = ROWS;
@@ -36,10 +43,10 @@ Matrix map_to_coo_matrix(map<pair<int,int>, float> m, int ROWS, int COLS) {
 	mat.val = (float*)malloc(sizeof(float)*size);
 
 	int i = 0;
-	map<pair<int, int>, float>::iterator it;
+	unordered_map<size_t, float>::iterator it;
 	for (it = m.begin(); it != m.end(); it++,i++) {
-		mat.I[i] = it->first.first;
-		mat.J[i] = it->first.second;
+		mat.I[i] = get_first(it->first);
+		mat.J[i] = get_second(it->first);
 		mat.val[i] = it->second;
 	}
 	return mat;
@@ -52,7 +59,7 @@ Matrix map_to_coo_matrix(map<pair<int,int>, float> m, int ROWS, int COLS) {
  */
 void esc(Matrix A, Matrix B) {
 	auto start = Clock::now();
-	map<pair<int, int>, float> C;
+	unordered_map<size_t, float> C;
 
 	for (int i = 0; i < A.N; i++) {
 		// ith column of A
